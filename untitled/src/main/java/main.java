c0476e351dc12d1e7cfbf4733f56f97bfe092658
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-12-25 20:49:02 +0200

    UP

--- untitled/src/main/java/main.java
+++ untitled/src/main/java/main.java
@@ -1,8 +1,15 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Arrays;
+
 public class main {
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/home/a1/Documents/chromedriver-linux64/chromedriver");
         WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.youtube.com/watch?v=RveELNiBD5Q");
+        System.out.println(driver.getTitle());
+        driver.close();
     }
 }
