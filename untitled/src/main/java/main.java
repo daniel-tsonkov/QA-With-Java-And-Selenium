76e68cfd211a0895c831d395210cf90802f1c7a7
dan <daniel_tzonkov@abv.bg>
2023-12-29 09:04:05 +0200

    up

--- untitled/src/main/java/main.java
+++ untitled/src/main/java/main.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 
 public class main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "/home/a1/Documents/chromedriver-linux64/chromedriver");
+        //System.setProperty("webdriver.chrome.driver", "/home/a1/Documents/chromedriver-linux64/chromedriver"); //for linux
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\dan\\Documents\\chromedriver-win64\\chromedriver.exe"); //for windows
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.youtube.com/watch?v=RveELNiBD5Q");
