4b621b06c373ab86c8d05e9d083e49e5bd234b1e
dan <daniel_tzonkov@abv.bg>
2023-12-29 09:54:48 +0200

    up

--- untitled/src/main/java/locators.java
+++ untitled/src/main/java/locators.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -6,5 +7,10 @@ public class locators {
         //System.setProperty("webdriver.chrome.driver", "/home/a1/Documents/chromedriver-linux64/chromedriver"); //for linux
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\dan\\Documents\\chromedriver-win64\\chromedriver.exe"); //for windows
         WebDriver driver = new ChromeDriver();
+
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+        driver.findElement(By.name("inputPassword")).sendKeys("rahul123");
+        driver.findElement(By.className("signInBtn")).click();
     }
 }
