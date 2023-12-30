ec8606542980f216f76e3d8e572c23723f76a978
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-12-30 21:35:51 +0200

    UP

--- untitled/src/main/java/locators.java
+++ untitled/src/main/java/locators.java
@@ -18,12 +18,16 @@ public class locators {
         System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
         driver.findElement(By.linkText("Forgot your password?")).click(); //open text link
         Thread.sleep(1000); //wait for stabilize single page application
+
         driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Dan"); //enter text in form
         driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("dan@abv.bg");
         //driver.findElement(By.cssSelector("input[placeholder='Email']")).clear();
         driver.findElement(By.xpath("//form/input[3]")).sendKeys("12345"); //telephone
         driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
         System.out.println(driver.findElement(By.cssSelector("form p")).getText());
+
+        driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]")).click();
+        Thread.sleep(1000); //wait for stabilize single page application
 
         driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul");
         driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
