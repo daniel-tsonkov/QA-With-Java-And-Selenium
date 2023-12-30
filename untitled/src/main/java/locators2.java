34424d4e1cbbdb549a8c088887fea450a98dd3f2
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-12-30 22:51:35 +0200

    UP

--- untitled/src/main/java/locators2.java
+++ untitled/src/main/java/locators2.java
@@ -7,13 +7,14 @@ import org.testng.Assert;
 
 public class locators2 {
     public static void main(String[] args) throws InterruptedException {
+        String name = "Dan";
         System.setProperty("webdriver.chrome.driver", "/home/a1/Documents/chromedriver-linux64/chromedriver"); //for linux
         //System.setProperty("webdriver.chrome.driver", "C:\\Users\\dan\\Documents\\chromedriver-win64\\chromedriver.exe"); //for windows
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); //wait 5 seconds after that report for error(no such element)
 
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
-        driver.findElement(By.id("inputUsername")).sendKeys("Dan"); //username
+        driver.findElement(By.id("inputUsername")).sendKeys(name); //username
         driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy"); //pass
         driver.findElement(By.className("signInBtn")).click(); //button
         Thread.sleep(1000);
@@ -21,6 +22,6 @@ public class locators2 {
         System.out.println(driver.findElement(By.tagName("p")).getText());
         Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
 
-        Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello Dan,");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello " + name + ",");
     }
 }
