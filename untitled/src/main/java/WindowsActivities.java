185e0659db53102bf2d5f20fb13a196d078e3402
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-01 22:01:05 +0200

    UP

--- untitled/src/main/java/WindowsActivities.java
+++ untitled/src/main/java/WindowsActivities.java
@@ -1,5 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WindowsActivities {
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/home/a1/Documents/chromedriver-linux64/chromedriver"); //for linux
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\dan\\Documents\\chromedriver-win64\\chromedriver.exe"); //for windows
+        WebDriver driver = new ChromeDriver();
+
 
     }
 }
