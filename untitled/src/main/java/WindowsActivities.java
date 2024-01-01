135c98dd4511114e3f946dec67e44181204cbfbc
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-01 22:34:22 +0200

    UP

--- untitled/src/main/java/WindowsActivities.java
+++ untitled/src/main/java/WindowsActivities.java
@@ -4,10 +4,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WindowsActivities {
     public static void main(String[] args) {
-        //System.setProperty("webdriver.chrome.driver", "/home/a1/Documents/chromedriver-linux64/chromedriver"); //for linux
+        //System.setProperty("webdriver.chrome.driver", "/home/a1/Documents/chromedriver-linux64/chromedriver"); //for linux run explicit version
         //System.setProperty("webdriver.chrome.driver", "C:\\Users\\dan\\Documents\\chromedriver-win64\\chromedriver.exe"); //for windows
         WebDriver driver = new ChromeDriver();
-        //WebDriver driver = new FirefoxDriver();
+        //WebDriver driver = new FirefoxDriver(); //problem in ubuntu
 
         driver.manage().window().maximize(); //run window in maximize mode
 
