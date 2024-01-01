d56d43abb5b40d1f4c394fbee8db34c4f31510a5
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-01 21:17:37 +0200

    UP

--- untitled/src/main/java/locators3.java
+++ untitled/src/main/java/locators3.java
@@ -1,5 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class locators3 {
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/home/a1/Documents/chromedriver-linux64/chromedriver"); //for linux
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\dan\\Documents\\chromedriver-win64\\chromedriver.exe"); //for windows
+        WebDriver driver = new ChromeDriver();
 
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
     }
 }
