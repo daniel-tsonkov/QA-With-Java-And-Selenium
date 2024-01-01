497c7109f1c0e4d2d97a642af4f703829df2e792
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-01 22:29:33 +0200

    UP

--- untitled/src/main/java/WindowsActivities.java
+++ untitled/src/main/java/WindowsActivities.java
@@ -1,11 +1,13 @@
 import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WindowsActivities {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "/home/a1/Documents/chromedriver-linux64/chromedriver"); //for linux
+        //System.setProperty("webdriver.chrome.driver", "/home/a1/Documents/chromedriver-linux64/chromedriver"); //for linux
         //System.setProperty("webdriver.chrome.driver", "C:\\Users\\dan\\Documents\\chromedriver-win64\\chromedriver.exe"); //for windows
         WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new FirefoxDriver();
 
         driver.manage().window().maximize(); //run window in maximize mode
 
@@ -13,5 +15,6 @@ public class WindowsActivities {
         driver.navigate().to("http://facebook.com"); //navigate to another website
         driver.navigate().back();
         driver.navigate().forward();
+
     }
 }
