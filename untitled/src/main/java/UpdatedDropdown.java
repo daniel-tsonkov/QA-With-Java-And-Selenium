38e8341b81ff4af1b87bd97a097503eb10ab193f
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-02 20:27:08 +0200

    UP

--- untitled/src/main/java/UpdatedDropdown.java
+++ untitled/src/main/java/UpdatedDropdown.java
@@ -3,12 +3,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class UpdatedDropdown {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize(); //run window in maximize mode
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
         driver.findElement(By.id("divpaxinfo")).click();
+        Thread.sleep(2000);
         driver.findElement(By.id("hrefIncAdt")).click();
+        driver.findElement(By.id("hrefIncAdt")).click();
+        driver.findElement(By.id("btnclosepaxoption")).click();
     }
 }
