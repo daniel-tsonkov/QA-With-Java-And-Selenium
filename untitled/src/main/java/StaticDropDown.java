2c68da986ce872a3bd799af029cd3c46e9e8dc34
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-02 20:03:06 +0200

    UP

--- untitled/src/main/java/StaticDropDown.java
+++ untitled/src/main/java/StaticDropDown.java
@@ -12,6 +12,9 @@ public class StaticDropDown {
 
         WebElement staticDropdown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
         Select dropdown = new Select(staticDropdown);
-        dropdown.selectByIndex(3); //select 3th element from dropdown menu
+        dropdown.selectByIndex(3); //select 3td element from dropdown menu
+        System.out.println(dropdown.getFirstSelectedOption().getText()); //get text from selected option
+        dropdown.selectByVisibleText("AED"); //another way to select
+        System.out.println(dropdown.getFirstSelectedOption().getText()); //get text from selected option
     }
 }
