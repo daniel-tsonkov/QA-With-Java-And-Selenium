548a9dd558687aa829d8e3299b75523d9819d461
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-02 20:20:38 +0200

    UP

--- untitled/src/main/java/UpdatedDropdown.java
+++ untitled/src/main/java/UpdatedDropdown.java
@@ -1,5 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class UpdatedDropdown {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize(); //run window in maximize mode
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
+        driver.findElement(By.id("divpaxinfo")).click();
+        driver.findElement(By.id("hrefIncAdt")).click();
     }
 }
