634ad5c6a8e9f27b94966bfe99d67af040266e36
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-03 20:57:52 +0200

    UP

--- untitled/src/main/java/Dropdown.java
+++ untitled/src/main/java/Dropdown.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Dropdown {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize(); //run window in maximize mode
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
@@ -11,5 +11,8 @@ public class Dropdown {
         //a[@value='BLR']
         //a[@value='MAA']
         driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+        driver.findElement(By.xpath("//a[@value='BLR']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
     }
 }
