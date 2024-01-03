775b68f1589a523aca3bcce36bede94ae297195e
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-03 21:28:16 +0200

    UP

--- untitled/src/main/java/Dropdown.java
+++ untitled/src/main/java/Dropdown.java
@@ -8,14 +8,13 @@ public class Dropdown {
         driver.manage().window().maximize(); //run window in maximize mode
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
-        //first way
-        //driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
-        //driver.findElement(By.xpath("//a[@value='BLR']")).click();
-        //Thread.sleep(1000);
-        //driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click(); //click second element
 
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+        driver.findElement(By.xpath("//a[@value='BLR']")).click();
+        Thread.sleep(1000);
+        //first way
+        //driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click(); //click second element
         //second way
-        driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='BLR']")).click();
         driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
 
 
