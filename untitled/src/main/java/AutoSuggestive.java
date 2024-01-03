93a58f40904d21e5a739d789aed1eb7c743ffcbb
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-03 21:57:36 +0200

    UP

--- untitled/src/main/java/AutoSuggestive.java
+++ untitled/src/main/java/AutoSuggestive.java
@@ -1,13 +1,19 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class AutoSuggestive {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize(); //run window in maximize mode
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
         driver.findElement(By.id("autosuggest")).sendKeys("ind");
+        Thread.sleep(3000);
+
+        List<WebElement> options = (List<WebElement>) driver.findElement(By.cssSelector("li[class='ui-menu-item'] a"));
     }
 }
