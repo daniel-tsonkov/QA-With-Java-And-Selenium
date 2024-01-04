238dde2405f28403d8a88d60a06f7681563a78df
dan <daniel_tzonkov@abv.bg>
2024-01-04 09:31:48 +0200

    up

--- untitled/src/main/java/UpdatedDropdown.java
+++ untitled/src/main/java/UpdatedDropdown.java
@@ -8,12 +8,15 @@ public class UpdatedDropdown {
         driver.manage().window().maximize(); //run window in maximize mode
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
+        driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount'")).click(); //lection No60
+
         driver.findElement(By.id("divpaxinfo")).click();
         Thread.sleep(2000);
 
         for (int i = 1; i < 5; i++) {
             driver.findElement(By.id("hrefIncAdt")).click();
         }
+
         driver.findElement(By.id("btnclosepaxoption")).click();
         System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
     }
