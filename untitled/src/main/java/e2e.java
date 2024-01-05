630bdabda8406909b6533a771cb3c0794b5791ae
dan <daniel_tzonkov@abv.bg>
2024-01-05 14:13:39 +0200

    up

--- untitled/src/main/java/e2e.java
+++ untitled/src/main/java/e2e.java
@@ -1,12 +1,24 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class e2e {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize(); //run window in maximize mode
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
+        driver.findElement(By.xpath("//a[@value='DLE']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
 
+        driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight")).click(); //select from calendar current date
+
+        if (driver.findElement(By.id("Div1")).getAttribute("style").contains("1")) { //update to 1
+            Assert.assertTrue(true);
+        } else {
+            Assert.assertTrue(false);
+        }
     }
 }
