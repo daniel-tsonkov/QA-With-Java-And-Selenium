df7d2ddd52499864e33a8633ec3abfcb9c685846
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-06 20:41:22 +0200

    UP

--- untitled/src/main/java/Assigment.java
+++ untitled/src/main/java/Assigment.java
@@ -11,6 +11,10 @@ public class Assigment {
 
         driver.findElement(By.id("name")).sendKeys(text);
         driver.findElement(By.id("alertbtn")).click(); //cssSelector - "[id='alertbtn']"
+
+        System.out.println(driver.switchTo().alert().getText());
         driver.switchTo().alert().accept(); //when work with POPUP
+
+        driver.close();
     }
 }
