50265b331e0905183b14d9f0859504fb07ebb801
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-06 20:25:51 +0200

    UP

--- untitled/src/main/java/Assigment.java
+++ untitled/src/main/java/Assigment.java
@@ -1,12 +1,14 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Assigment {
     public static void main(String[] args) {
+        String text = "Dan";
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize(); //run window in maximize mode
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
-
+        driver.findElement(By.id("name")).sendKeys(text);
     }
 }
