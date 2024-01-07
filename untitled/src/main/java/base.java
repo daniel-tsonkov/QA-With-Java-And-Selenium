914dd3c820fb957f65f72095f48b9e7ad25176b5
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-07 22:22:10 +0200

    UP

--- untitled/src/main/java/base.java
+++ untitled/src/main/java/base.java
@@ -11,6 +11,19 @@ public class base {
         driver.manage().window().maximize(); //run window in maximize mode
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 
-        List<WebElement> products = driver.findElements (By.cssSelector("h4.product-name"));
+        List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+
+        //for (WebElement product : products){
+        for (int i = 0; i < products.size(); i++) {
+            //System.out.println(product.getText());
+            //String name = products.getText();
+            String name = products.get(i).getText();
+
+            if (name.contains("Cucumber")) {
+                driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+            }
+        }
+
+        //driver.close();
     }
 }
