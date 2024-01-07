3d07cbf34e6de5a609323e38ed429af18e26e58a
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-07 21:42:28 +0200

    UP

--- untitled/src/main/java/base.java
+++ untitled/src/main/java/base.java
@@ -1,2 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class base {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize(); //run window in maximize mode
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+
+
+    }
 }
