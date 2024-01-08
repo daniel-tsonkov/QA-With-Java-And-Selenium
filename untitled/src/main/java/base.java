bb57d061036e9dcf57c44b3abf9cc3fb9e204385
dan <daniel_tzonkov@abv.bg>
2024-01-08 09:59:02 +0200

    up

--- untitled/src/main/java/base.java
+++ untitled/src/main/java/base.java
@@ -13,20 +13,18 @@ public class base {
         driver.manage().window().maximize(); //run window in maximize mode
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 
-        String[] itemsNeeded = {"Cucumber", "Brocolli", "Beetroot", "Beans", "Apple"}; //add more items
+        String[] itemsNeeded = {"Brocolli", "Cucumber", "Beetroot"}; //add more items
 
         List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
 
-        //for (WebElement product : products){
         for (int i = 0; i < products.size(); i++) {
-            //System.out.println(product.getText());
-            //String name = products.getText();
-            String[] name = products.get(i).getText().split(" - ");
+            String[] name = products.get(i).getText().split("-");
+            String formattedName = name[0].trim();
             System.out.println(name[0]);
 
             List<String> itemsNeededList = Arrays.asList(itemsNeeded); //array to listArrays
 
-            if (itemsNeededList.contains(name)) {
+            if (itemsNeededList.contains(formattedName)) {
                 driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
             }
         }
