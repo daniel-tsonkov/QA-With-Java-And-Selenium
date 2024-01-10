0e03c400ba709cdf369eb2fa088289dd49341107
dan <daniel_tzonkov@abv.bg>
2024-01-10 10:02:10 +0200

    up

--- untitled/src/main/java/fluentWaitTest.java
+++ untitled/src/main/java/fluentWaitTest.java
@@ -1,6 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
 
 public class fluentWaitTest {
     public static void main(String[] args) {
@@ -9,6 +13,9 @@ public class fluentWaitTest {
         driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
 
         driver.findElement(By.xpath("//button[contains(text(),'Start')]")).click();
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(3));
 
     }
 }
