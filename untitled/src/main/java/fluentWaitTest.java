cbe7a831d3383eb5a2098956f7d16ebafcbcc48f
dan <daniel_tzonkov@abv.bg>
2024-01-10 10:17:37 +0200

    up

--- untitled/src/main/java/fluentWaitTest.java
+++ untitled/src/main/java/fluentWaitTest.java
@@ -1,10 +1,13 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 
 import java.time.Duration;
+import java.util.function.Function;
 
 public class fluentWaitTest {
     public static void main(String[] args) {
@@ -14,8 +17,14 @@ public class fluentWaitTest {
 
         driver.findElement(By.xpath("//button[contains(text(),'Start')]")).click();
         Wait<WebDriver> wait = new FluentWait<>(driver)
-                .withTimeout(Duration.ofSeconds(30))
-                .pollingEvery(Duration.ofSeconds(3));
+                .withTimeout(Duration.ofSeconds(30)) //for selenium 4.xx
+                .pollingEvery(Duration.ofSeconds(3)) //for selenium 4.xx
+                .ignoring(NoSuchElementException.class);
 
+        WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.cssSelector("[id='finish'] h4"));
+            }
+        });
     }
 }
