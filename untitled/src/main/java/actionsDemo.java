98624a75d4545abb114576c37624f0616db0f1d2
dan <daniel_tzonkov@abv.bg>
2024-01-11 09:56:21 +0200

    up

--- untitled/src/main/java/actionsDemo.java
+++ untitled/src/main/java/actionsDemo.java
@@ -1,12 +1,17 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class actionsDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize(); //run window in maximize mode
         driver.get("https://www.amazon.com/");
+        //WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(20)); //after on Selenium 4.xx
 
-
+        Thread.sleep(20000);
+        Actions a = new Actions(driver);
+        a.moveToElement(driver.findElement(By.cssSelector("a[id='nav-link-accountList']"))).build().perform();
     }
 }
