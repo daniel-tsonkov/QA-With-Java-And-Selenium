441199ef6dffbd05e1d50d434f69bc64dcb42fa2
dan <daniel_tzonkov@abv.bg>
2024-01-11 14:52:16 +0200

    up

--- untitled/src/main/java/actionsDemo.java
+++ untitled/src/main/java/actionsDemo.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -15,12 +14,12 @@ public class actionsDemo {
         driver.get("https://www.amazon.com/");
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
 
-        //Thread.sleep(20000);
         Actions a = new Actions(driver);
         WebElement move = driver.findElement(By.cssSelector("a[id='nav-link-accountList']")); //local variable
         a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").build().perform();
         //build() creat operation
         //performe() execute operation
+        a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).doubleClick().build().perform(); //doble click fro select text
         a.moveToElement(move).build().perform();
 
         System.exit(0);
