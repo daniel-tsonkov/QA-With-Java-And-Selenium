905a662392191c9cd8b2566cfad4240690231191
dan <daniel_tzonkov@abv.bg>
2024-01-11 14:57:23 +0200

    up

--- untitled/src/main/java/actionsDemo.java
+++ untitled/src/main/java/actionsDemo.java
@@ -16,11 +16,13 @@ public class actionsDemo {
 
         Actions a = new Actions(driver);
         WebElement move = driver.findElement(By.cssSelector("a[id='nav-link-accountList']")); //local variable
-        a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").build().perform();
+        WebElement moveToSearch = driver.findElement(By.id("twotabsearchtextbox"));
+        a.moveToElement(moveToSearch).click().keyDown(Keys.SHIFT).sendKeys("hello").build().perform();
         //build() creat operation
         //performe() execute operation
-        a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).doubleClick().build().perform(); //doble click fro select text
+        a.moveToElement(moveToSearch).doubleClick().build().perform(); //doble click fro select text
         a.moveToElement(move).build().perform();
+        a.moveToElement(move).contextClick().build().perform(); //contextClick() - RIGHT click og mouse
 
         System.exit(0);
     }
