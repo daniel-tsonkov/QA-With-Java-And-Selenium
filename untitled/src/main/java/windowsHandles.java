fc3e1a5a2022f7cf761fe8ffdefbb39e7f299479
dan <daniel_tzonkov@abv.bg>
2024-01-11 15:03:50 +0200

    up

--- untitled/src/main/java/windowsHandles.java
+++ untitled/src/main/java/windowsHandles.java
@@ -1,5 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class windowsHandles {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize(); //run window in maximize mode
+        driver.get("https://rahulshettyacademy.com/loginpagePractise/#");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
 
+
+        System.exit(0);
     }
 }
