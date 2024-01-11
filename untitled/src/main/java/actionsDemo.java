26d9692f32e8ea896a1ae6c2d54fc8051eddfb4f
dan <daniel_tzonkov@abv.bg>
2024-01-11 14:48:15 +0200

    up

--- untitled/src/main/java/actionsDemo.java
+++ untitled/src/main/java/actionsDemo.java
@@ -4,20 +4,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class actionsDemo {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize(); //run window in maximize mode
         driver.get("https://www.amazon.com/");
-        //WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(20)); //after on Selenium 4.xx
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
 
-        Thread.sleep(20000);
+        //Thread.sleep(20000);
         Actions a = new Actions(driver);
         WebElement move = driver.findElement(By.cssSelector("a[id='nav-link-accountList']")); //local variable
         a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").build().perform();
         //build() creat operation
         //performe() execute operation
         a.moveToElement(move).build().perform();
+
+        System.exit(0);
     }
 }
