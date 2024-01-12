5839236b056b873bc2b3c3d2b9757e2208f3ef39
dan <daniel_tzonkov@abv.bg>
2024-01-12 10:48:44 +0200

    up

--- untitled/src/main/java/scope.java
+++ untitled/src/main/java/scope.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,5 +10,9 @@ public class scope {
         driver.manage().window().maximize(); //run window in maximize mode
         driver.get("https://rahulshettyacademy.com/AutomationPractice/"); // https://rahulshettyacademy.com/AutomationPractice/
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
+
+        System.out.println(driver.findElements((By.tagName("a"))).size());
+
+        System.exit(0);
     }
 }
