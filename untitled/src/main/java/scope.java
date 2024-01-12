0d2f7cb95d747c740ffa8372ab785f4863eb1fa9
dan <daniel_tzonkov@abv.bg>
2024-01-12 11:02:10 +0200

    up

--- untitled/src/main/java/scope.java
+++ untitled/src/main/java/scope.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -11,7 +12,10 @@ public class scope {
         driver.get("https://rahulshettyacademy.com/AutomationPractice/"); // https://rahulshettyacademy.com/AutomationPractice/
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
 
-        System.out.println(driver.findElements((By.tagName("a"))).size());
+        System.out.println(driver.findElements((By.tagName("a"))).size()); // all a tagname in page
+
+        WebElement footerDriver = driver.findElement(By.id("gf-BIG")); // scope to a tagname  only in scope
+        System.out.println(footerDriver.findElements((By.tagName("a"))).size()); // only in footer
 
         System.exit(0);
     }
