84b1d8c98e5ea6a2e7e3eabd679cec2e63b73e43
dan <daniel_tzonkov@abv.bg>
2024-01-12 10:32:04 +0200

    up

--- untitled/src/main/java/scope.java
+++ untitled/src/main/java/scope.java
@@ -1,5 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class scope {
     public static void main(String[] args) {
-
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize(); //run window in maximize mode
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/"); // https://rahulshettyacademy.com/AutomationPractice/
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
     }
 }
