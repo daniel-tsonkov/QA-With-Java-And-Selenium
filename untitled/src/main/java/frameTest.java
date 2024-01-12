6a747912c2b0c1ab708f4d8760c6c761d99ac6ab
dan <daniel_tzonkov@abv.bg>
2024-01-12 09:51:32 +0200

    up

--- untitled/src/main/java/frameTest.java
+++ untitled/src/main/java/frameTest.java
@@ -1,7 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.interactions.Actions;
 import java.time.Duration;
 
 public class frameTest {
@@ -13,6 +14,12 @@ public class frameTest {
 
         driver.switchTo().frame(driver.findElement(By.cssSelector("iframe.demo-frame")));
         driver.findElement(By.id("draggable")).click();
+
+        Actions action = new Actions(driver);
+        WebElement source = driver.findElement(By.id("draggable"));
+        WebElement target = driver.findElement(By.id("droppable"));
+
+        action.dragAndDrop(source, target).build().perform();
 
         System.exit(0);
     }
