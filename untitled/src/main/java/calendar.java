131f713937250301767b26ac6be09be240bd7d22
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-14 20:35:39 +0200

    UP

--- untitled/src/main/java/calendar.java
+++ untitled/src/main/java/calendar.java
@@ -1,15 +1,24 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import javax.lang.model.element.Element;
 import java.time.Duration;
+import java.util.List;
 
 public class calendar {
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new ChromeDriver();
+        ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize(); //run window in maximize mode
         driver.get("https://www.path2usa.com/travel-companions");
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
 
+        //driver.findElement(By.id("form-field-travel_comp_date")).click();
+        driver.findElement(By.xpath(".//*[@id='form-field-travel_comp_date']")).click();
+        List<WebElement> dates = driver.findElements(By.name(".day"));
 
+        System.exit(0);
     }
 }
