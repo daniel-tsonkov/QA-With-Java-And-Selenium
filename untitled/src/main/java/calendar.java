ee61795083faa5553886ad7f37a2505c13512c82
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-14 20:07:58 +0200

    UP

--- untitled/src/main/java/calendar.java
+++ untitled/src/main/java/calendar.java
@@ -1,5 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class calendar {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize(); //run window in maximize mode
+        driver.get("https://www.path2usa.com/travel-companions");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
+
 
     }
 }
