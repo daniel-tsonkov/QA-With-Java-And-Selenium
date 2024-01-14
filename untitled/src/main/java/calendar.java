07df0b6073c8287d1a27ab54fb8360c4bf183542
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-01-14 21:33:16 +0200

    UP

--- untitled/src/main/java/calendar.java
+++ untitled/src/main/java/calendar.java
@@ -1,14 +1,15 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
-import javax.lang.model.element.Element;
 import java.time.Duration;
 import java.util.List;
 
 public class calendar {
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/home/a1/Documents/chromedriver-linux64/chromedriver"); //for linux
         //WebDriver driver = new ChromeDriver();
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize(); //run window in maximize mode
@@ -16,7 +17,11 @@ public class calendar {
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
 
         //driver.findElement(By.id("form-field-travel_comp_date")).click();
-        driver.findElement(By.xpath(".//*[@id='form-field-travel_comp_date']")).click();
+        //driver.findElement(By.xpath(".//*[@id='form-field-travel_comp_date']")).click();
+        WebElement element = driver.findElement(By.xpath(".//*[@id='form-field-travel_comp_date']"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).click().build().perform();
+
         List<WebElement> dates = driver.findElements(By.className("day"));
 
         int count = driver.findElements(By.className("day")).size();
