3ee4e86e07be6e1973cc2d9686e410ebfe8c4009
dan <daniel_tzonkov@abv.bg>
2024-01-16 15:33:54 +0200

    up

--- untitled/src/main/java/sslCheck.java
+++ untitled/src/main/java/sslCheck.java
@@ -1,11 +1,15 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
 public class sslCheck {
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.setAcceptInsecureCerts(true);
+
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize(); //run window in maximize mode
         driver.get("https://expired.badssl.com/");
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
