a5e4026d8edae86223acdedc1afe7a8937bbb9b6
dan <daniel_tzonkov@abv.bg>
2024-01-16 15:42:57 +0200

    up

--- untitled/src/main/java/sslCheck.java
+++ untitled/src/main/java/sslCheck.java
@@ -7,7 +7,16 @@ import java.time.Duration;
 public class sslCheck {
     public static void main(String[] args) {
         ChromeOptions options = new ChromeOptions();
+        //EdgeOptions options1 = new EdgeOptions(); // for Edge
         options.setAcceptInsecureCerts(true);
+        //options1.setAcceptInsecureCerts(true);
+
+        /*WebDriver driver1 = new EdgeDriver(options1);
+        driver1.manage().window().maximize(); //run window in maximize mode
+        driver1.get("https://expired.badssl.com/");
+        driver1.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
+
+        System.out.println(driver1.getTitle());*/
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize(); //run window in maximize mode
