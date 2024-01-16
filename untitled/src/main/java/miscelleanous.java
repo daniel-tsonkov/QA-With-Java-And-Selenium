a93a39d2d88e4b6177812b5aff6e47c460526db8
dan <daniel_tzonkov@abv.bg>
2024-01-16 16:29:36 +0200

    up

--- untitled/src/main/java/miscelleanous.java
+++ untitled/src/main/java/miscelleanous.java
@@ -1,6 +1,9 @@
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
 import java.time.Duration;
 
 public class miscelleanous {
@@ -12,7 +15,7 @@ public class miscelleanous {
         driver.get("https://abv.bg/");
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
 
-
+        File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE); //create screenshot on window
 
         System.exit(0);
     }
