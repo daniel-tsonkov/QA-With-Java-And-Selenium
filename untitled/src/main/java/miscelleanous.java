8ecc38f1cea63f0ba540a401aa70922db651016d
dan <daniel_tzonkov@abv.bg>
2024-01-16 16:09:21 +0200

    up

--- untitled/src/main/java/miscelleanous.java
+++ untitled/src/main/java/miscelleanous.java
@@ -7,7 +7,12 @@ public class miscelleanous {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize(); //run window in maximize mode
-        driver.get("https://jqueryui.com/droppable/");
+        //driver.manage().window().fullscreen();
+        driver.get("https://abv.bg/");
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
+
+
+
+        System.exit(0);
     }
 }
