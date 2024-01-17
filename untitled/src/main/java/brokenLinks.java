793a2683d0d796acdc3eb3916ee94469123bbfb9
dan <daniel_tzonkov@abv.bg>
2024-01-17 16:52:27 +0200

    up

--- untitled/src/main/java/brokenLinks.java
+++ untitled/src/main/java/brokenLinks.java
@@ -1,5 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class brokenLinks {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
 
+
+
+
+        System.exit(0);
     }
 }
