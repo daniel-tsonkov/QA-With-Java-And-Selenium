ab3af73527110fa09e6105aef809c2d2669e63b1
dan <daniel_tzonkov@abv.bg>
2024-01-18 09:19:06 +0200

    up

--- untitled/src/main/java/brokenLinks.java
+++ untitled/src/main/java/brokenLinks.java
@@ -2,10 +2,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.time.Duration;
 
 public class brokenLinks {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://rahulshettyacademy.com/AutomationPractice");
@@ -13,6 +18,8 @@ public class brokenLinks {
 
         String url = driver.findElement(By.cssSelector("a[href*='soapui']")).getAttribute("href");
         System.out.println(url);
+
+        HttpURLConnection connection = (HttpURLConnection)new URL(url).openConnection();
 
         System.exit(0);
     }
