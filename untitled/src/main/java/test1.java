78307bd11c1f6b93f811de73167b6066d9ab2994
dan <daniel_tzonkov@abv.bg>
2024-01-23 16:21:34 +0200

    23012024

--- untitled/src/main/java/test1.java
+++ untitled/src/main/java/test1.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.ArrayList;
 
 public class test1 {
     public static void main(String[] args) {
@@ -10,5 +11,12 @@ public class test1 {
         driver.get("https://jqueryui.com/droppable/");
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
 
+        ArrayList<String> string = new ArrayList<>();
+        string.add("asd");
+
+        System.out.println(string.stream().filter(s -> s.startsWith("a")).count());
+
+        driver.close();
+        System.exit(0);
     }
 }
