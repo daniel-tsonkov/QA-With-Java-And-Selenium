c6dee60a8c03334fb13a69d7bfdec9ec7c9df48c
dan <daniel_tzonkov@abv.bg>
2024-01-23 15:46:41 +0200

    UP

--- untitled/src/main/java/test1.java
+++ untitled/src/main/java/test1.java
@@ -1,5 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class test1 {
     public static void main(String[] args) {
-
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize(); //run window in maximize mode
+        driver.get("https://jqueryui.com/droppable/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
     }
 }
