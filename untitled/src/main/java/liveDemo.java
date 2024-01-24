e4fb0cea9dd00acbccabdbef7e8816d9b5b56e84
dan <daniel_tzonkov@abv.bg>
2024-01-24 14:31:20 +0200

    24012024

--- untitled/src/main/java/liveDemo.java
+++ untitled/src/main/java/liveDemo.java
@@ -1,5 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class liveDemo {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize(); //run window in maximize mode
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
 
+
+
+        System.exit(0);
     }
 }
