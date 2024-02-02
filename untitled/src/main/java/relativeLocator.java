d0b752a7bc9d060a2ed4305c75eb66b3f82674c6
dan <daniel_tzonkov@abv.bg>
2024-02-02 11:20:16 +0200

    02022024

---
+++ untitled/src/main/java/relativeLocator.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class relativeLocator {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize(); //run window in maximize mode
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(20000));
+
+
+
+        System.exit(0);
+    }
+}
