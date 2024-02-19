e77d5020027b5fd77fdf64028bf98498292cc917
dan <daniel_tzonkov@abv.bg>
2024-02-19 11:53:46 +0200

    19022024

--- untitled/src/test/DayTwoTest.java
+++ untitled/src/test/DayTwoTest.java
@@ -1,8 +1,14 @@
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class DayTwoTest {
     @Test
     public void ploan() {
         System.out.println("AAA");
+    }
+
+    @BeforeTest
+    public void prerequister() {
+
     }
 }
---
+++ untitled/src/test/zzz/DayFourTest.java
@@ -0,0 +1,25 @@
+package zzz;
+
+import org.testng.annotations.Test;
+
+public class DayFourTest {
+    @Test
+    public void WebLoginHomeLoan() {
+        System.out.println("webloginhome");
+    }
+
+    @Test
+    public void MobileLoginHomeLoan() {
+        System.out.println("Mobileloginhome");
+    }
+
+    @Test
+    public void MobileLogOutHome() {
+        System.out.println("MobilelogOut");
+    }
+
+    @Test
+    public void LoginApiHomeLoan() {
+        System.out.println("loginapihomeloan");
+    }
+}
