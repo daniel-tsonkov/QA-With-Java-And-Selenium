b7cc7c65fa3ec88116732f221bab3a836877ad08
dan <daniel_tzonkov@abv.bg>
2024-02-19 10:04:23 +0200

    19022024

---
+++ untitled/src/test/java/DayFourTest.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class DayFourTest {
+    @Test
+    public void WebLoginHomeLoan() {
+        System.out.println("webloginhome");
+    }
+
+    @Test
+    public void MobileLoginHomeLoan() {
+        System.out.println("mobileloginhome");
+    }
+
+    @Test
+    public void LoginApiHomeLoan() {
+        System.out.println("loginapihomeloan");
+    }
+}
