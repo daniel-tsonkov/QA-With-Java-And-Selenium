adeafb6cd423e7ad10a8aee2cd758fdb67bcf778
dan <daniel_tzonkov@abv.bg>
2024-02-19 15:39:16 +0200

    19022024

--- untitled/src/test/DayCTest.java
+++ untitled/src/test/DayCTest.java
@@ -1,3 +1,4 @@
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -5,7 +6,12 @@ import org.testng.annotations.Test;
 public class DayCTest {
     @Test
     public void ploa() {
-        System.out.println("Day C");
+        System.out.println("Day C1");
+    }
+
+    @Test
+    public void ploaa() {
+        System.out.println("Day C2");
     }
 
     @BeforeSuite
@@ -15,6 +21,11 @@ public class DayCTest {
 
     @BeforeMethod
     public void beforeMethod() {
-        System.out.println("Before method From C");
+        System.out.println("Before every method in this clas from C");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("After every method in this clas from C");
     }
 }
